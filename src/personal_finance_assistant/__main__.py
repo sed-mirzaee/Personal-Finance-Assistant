4b848d9fbbfb61f477dbeac814ce2c6861dcd305
Your Name <sed.mirzaee@gmail.com>
2026-09-20 23:39:57 +0200

    feat: add CLI command stubs and entry points

---
+++ src/personal_finance_assistant/__main__.py
@@ -0,0 +1,8 @@
+
+# Entry point of project
+def main():
+    # Call cli
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ src/personal_finance_assistant/cli.py
@@ -0,0 +1,40 @@
+def cmd_show_recommendations():
+    pass
+
+def cmd_get_file():
+    pass
+
+def cmd_get_transactions():
+    pass
+
+def cmd_predict_balance():
+    pass
+
+def cmd_show_summary():
+    pass
+
+def cmd_set_balance():
+    pass
+
+def cmd_get_balance():
+    pass
+
+def cmd_add_transaction():
+    pass
+
+def cmd_show_help():
+    pass
+
+def cmd_settings():
+    pass
+
+def run_main_menu():
+    pass
+    # a always true loop for getting command and run commands until exit
+
+# Entry point of project.scripts
+def main():
+    main_menu()
+
+if __name__ == "__main__":
+    main()
